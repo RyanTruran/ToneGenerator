95ddafdd9ed401107c6136218f8ef57923282453
Ryan <ryan.truran@gmail.com>
2020-02-17 09:17:18 -0600

    updated comments in ButtonPressCommand

--- ToneGenerator/Commands/ButtonPressCommand.cs
+++ ToneGenerator/Commands/ButtonPressCommand.cs
@@ -13,20 +13,24 @@ namespace ToneGenerator.Commands
         {
             _ViewModel = viewModel;
         }
-
+        /// <summary>
+        /// Verifies that the button press can still be executed prior to execution.
+        /// </summary>
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
-
+        //
         public bool CanExecute(object parameter)
         {
+            //only allow button press if a connection has been established.
             return _ViewModel.Connected;
         }
 
         public void Execute(object parameter)
         {
+            //Gets the Content from the button, to play as a tone, then passes it to the SendNote Command
             _ViewModel.SendNote(parameter.ToString());
         }
     }
